98241c1536fd53152f185323af55f30a0622ed5e
sukritiistired <sukritibaryal23@gmail.com>
2026-03-08 11:36:26 +0545

    content can be added from media mgmt in blog outside folder

--- blog/forms.py
+++ blog/forms.py
@@ -2,28 +2,26 @@ from django import forms
 from .models import Blog
 from contentmgmt.models import MediaFile
 
-
 class BlogForm(forms.ModelForm):
     featured_image = forms.ModelChoiceField(
-        queryset=MediaFile.objects.all(),  # Only images from CMS
+        queryset=MediaFile.objects.all(),
         required=False,
-        widget=forms.Select(attrs={'class': 'form-select'})
+        widget=forms.HiddenInput()  # CMS modal will populate this
     )
 
     class Meta:
         model = Blog
-        fields = ['title', 'subtitle', 'slug', 'content', 'featured_image',
-                  'homepage', 'active', 'meta_title', 'meta_keywords', 'meta_description']
+        fields = [
+            'title', 'subtitle', 'slug', 'content', 'featured_image',
+            'homepage', 'active', 'meta_title', 'meta_keywords', 'meta_description'
+        ]
         widgets = {
-            # This keeps the field in the form but hides it from the user's eyes
             'homepage': forms.HiddenInput(),
-            'slug': forms.TextInput(attrs={
-                'id': 'id_slug'
-            }),
-            'content': forms.Textarea(attrs={'id': 'id_content'})
+            'slug': forms.TextInput(attrs={'id': 'id_slug'}),
+            'content': forms.Textarea(attrs={'id': 'id_content'}),
         }
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            if self.instance.pk:
-                self.fields['slug'].widget.attrs['data-blog-id'] = self.instance.pk
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['slug'].widget.attrs['data-blog-id'] = self.instance.pk
